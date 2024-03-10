5ce664742e0678737ad9ccd189e78b55c97a09a9
Shun Git <kwokshungit@gmail.com>
2024-03-10 15:47:23 +0800

    update install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+files=$(cat <<EOF
+zshenv
+zshrc
+vimrc
+lesskey
+tmux.conf
+gitconfig
+gitignore
+EOF
+)
+
+ddir="$(dirname $0)"
+
+# Symlink to default locations
+for f in $files; do
+	fn="$ddir/$f"
+	[ -z "$fn" ] && continue
+	ln -vs --backup=existing $fn $HOME/.$fn
+done
--- tools/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Symlink to default locations
-for fn in $(cat $(dirname $0)/locations); do
-	[ -z "$fn" ] && continue
-	ln -vs --backup=existing $PWD/$fn $HOME/.$fn
-done
